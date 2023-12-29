55c4a1546142818a3edc12cc50cbde8f1694914d
alexxgusse <gusev709.com@yandex.ru>
2023-12-29 15:33:16 +0500

    added a table model for a group of students, configured an admin panel for the admin application

--- eduportal/users/admin.py
+++ eduportal/users/admin.py
@@ -1,6 +1,27 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 
-from users.models import User
+from users.models import User, CategoryUser, GroupStudents
 
-admin.site.register(User, UserAdmin)
+# admin.site.register(User, UserAdmin)
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    fields = ['password', 'photo', 'last_login', 'username', 'first_name', 'last_name', 'email',
+              'date_birth', 'cat_user', 'group_stud'
+              ]
+    list_display = ('first_name', 'last_name', 'username', 'group_stud')
+    list_display_links = ('first_name', 'username')
+
+
+@admin.register(CategoryUser)
+class CategoryUserAdmin(admin.ModelAdmin):
+    fields = ['title', ]
+
+
+@admin.register(GroupStudents)
+class GroupStudentsAdmin(admin.ModelAdmin):
+    fields = ['group', 'slug']
+    prepopulated_fields = {"slug": ("group",)}
+    list_display = ('group', 'slug')
--- eduportal/users/models.py
+++ eduportal/users/models.py
@@ -7,7 +7,10 @@ class User(AbstractUser):
     photo = models.ImageField(upload_to="users/%Y/%m/%d/", blank=True, null=True, verbose_name="Фотография")
     date_birth = models.DateTimeField(blank=True, null=True, verbose_name="Дата рождения")
     cat_user = models.ForeignKey('CategoryUser', on_delete=models.PROTECT, related_name='users',
-                                 default=None, null=True)
+                                 default=None, null=True, verbose_name='Категория пользователя')
+    group_stud = models.ForeignKey('GroupStudents', blank=True, related_name='groups',
+                                        verbose_name='Группа студента', on_delete=models.PROTECT, default=None,
+                                   null=True)
 
     def __str__(self):
         return self.username
@@ -22,3 +25,15 @@ class CategoryUser(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class GroupStudents(models.Model):
+    class Meta:
+        verbose_name = 'Группы студентов'
+        verbose_name_plural = 'Группа студентов'
+
+    group = models.CharField(max_length=100, db_index=True, verbose_name='Группа')
+    slug = models.SlugField(max_length=100, unique=True, db_index=True, verbose_name='Slug')
+
+    def __str__(self):
+        return self.group
